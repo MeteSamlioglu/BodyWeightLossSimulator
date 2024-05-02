4fbf22d491f589d104030ad6209a17691960aee3
Mete <60179489+MeteSamlioglu@users.noreply.github.com>
2024-05-02 14:59:55 +0300

    Photo uploading box is added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import * as tf from "@tensorflow/tfjs";
 import * as bodyPix from "@tensorflow-models/body-pix";
 import "./App.css";
@@ -6,85 +6,112 @@ import "./App.css";
 function App() {
   const imageRef = useRef(null);
   const canvasRef = useRef(null);
+  const [imageURL, setImageURL] = useState(null); // State to hold the uploaded image URL
 
-  const runBodysegment = async () => {
-    const net = await bodyPix.load();
-    console.log("BodyPix model loaded.");
-    detect(net);
+  const handleImageUpload = event => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+      setImageURL(e.target.result);
+    };
+
+    reader.readAsDataURL(file);
   };
 
-  const detect = async (net) => {
+  const runBodysegment = async () => {
     if (imageRef.current) {
-      const image = imageRef.current;
-      const imageWidth = image.width;
-      const imageHeight = image.height;
-
-      // Set canvas height and width
-      canvasRef.current.width = imageWidth;
-      canvasRef.current.height = imageHeight;
-      console.log(imageWidth)
-      console.log(imageHeight)
-      // Make detections
-      const person = await net.segmentPersonParts(image);
-      console.log(person.allPoses);
-
-      // Visualization code
-      const coloredPartImage = bodyPix.toColoredPartMask(person);
-      const opacity = 0.7;
-      const flipHorizontal = false;
-      const maskBlurAmount = 0;
-      const canvas = canvasRef.current;
-
-      bodyPix.drawMask(
-        canvas,
-        image,
-        coloredPartImage,
-        opacity,
-        maskBlurAmount,
-        flipHorizontal
-      );
+      const net = await bodyPix.load();
+      console.log("BodyPix model loaded.");
+      detect(net);
     }
   };
 
+  const detect = async (net) => {
+    const image = imageRef.current;
+    const imageWidth = image.width;
+    const imageHeight = image.height;
+
+    // Set canvas height and width
+    canvasRef.current.width = imageWidth;
+    canvasRef.current.height = imageHeight;
+
+    // Make detections
+    const person = await net.segmentPersonParts(image);
+    console.log(person.allPoses);
+
+    // Visualization code
+    const coloredPartImage = bodyPix.toColoredPartMask(person);
+    const opacity = 0.7;
+    const flipHorizontal = false;
+    const maskBlurAmount = 0;
+    const canvas = canvasRef.current;
+
+    bodyPix.drawMask(
+      canvas,
+      image,
+      coloredPartImage,
+      opacity,
+      maskBlurAmount,
+      flipHorizontal
+    );
+  };
+
   useEffect(() => {
-    runBodysegment();
-  }, []);
+    if (imageURL) {
+      runBodysegment();
+    }
+  }, [imageURL]); // Re-run the effect when imageURL changes
 
   return (
     <div className="App">
       <header className="App-header">
-        <img
-          ref={imageRef}
-          src="sample.png"
-          alt="Sample"
-          onLoad={() => runBodysegment()}
+        <input 
+          type="file" 
+          onChange={handleImageUpload} 
+          accept="image/*" 
           style={{
             position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
+            top: 0,
             left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
+            zIndex: 10,
+            width: '100%',
+            opacity: 1,
+            cursor: 'pointer',
+          }}
+        />
+        {imageURL && (
+          <div style={{
+            position: "relative",
             maxWidth: "100%",
             maxHeight: "100%",
-          }}
-        />
-        <canvas
-          ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: 0,
-            right: 0,
-            textAlign: "center",
-            zindex: 9,
-          }}
-        />
+          }}>
+            <img
+              ref={imageRef}
+              src={imageURL}
+              alt="Upload Preview"
+              onLoad={runBodysegment}
+              style={{
+                maxWidth: "100%",
+                maxHeight: "100%",
+                display: "block", // Ensure it fills the container
+              }}
+            />
+            <canvas
+              ref={canvasRef}
+              style={{
+                position: "absolute",
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: '100%',
+              }}
+            />
+          </div>
+        )}
       </header>
     </div>
-  );
+  ); 
 }
 
-export default App;
+export default App;
